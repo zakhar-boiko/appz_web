9e3b9685c6e351a5b496b3701ca33f1e3045321e
Artem Huk <artem.huk.si@gmail.com>
2023-12-10 23:38:33 +0200

    add something

--- src/features/statistics/api/client.ts
+++ src/features/statistics/api/client.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from "react-query";
 import api from "../../../services/api/api";
-import { ShortPollType } from "./types";
+import { BigStatisticType, ShortPollType } from "./types";
 
 export const useAllPallsQuery = (userId: string) => {
   return useQuery(["all-palls", userId], () => {
@@ -10,3 +10,26 @@ export const useAllPallsQuery = (userId: string) => {
   });
 };
 
+  // Define the function to fetch data from the endpoint
+const fetchDataByPoll = async (pollId: string, filterId: string | null, userId: string) => {
+    // Construct the query parameters
+    const params = new URLSearchParams({
+      pollId: pollId,
+      userId: userId,
+    });
+  
+    // Add filterId to the query parameters only if it is not null
+    if (filterId !== null) {
+      params.append('filterId', filterId);
+    }
+  
+    // Make the GET request using the api instance
+    return await api.get<BigStatisticType,BigStatisticType>(`/statistic?${params.toString()}`);
+  };
+  
+  // Define the hook to use the fetch function
+  export const useDataByPollQuery = (pollId: string, filterId: string | null, userId: string) => {
+    return useQuery(['data-by-poll', pollId, filterId, userId], () =>
+      fetchDataByPoll(pollId, filterId, userId)
+    );
+  };
--- src/features/statistics/api/types.ts
+++ src/features/statistics/api/types.ts
@@ -4,3 +4,22 @@ export type ShortPollType = {
     pollDate: string;
   };
   
+
+  export type  BigStatisticType = {
+    filterId: number;
+    titleOfPoll: string;
+    statistic: StatisticType;
+    questionBlockSet: QuestionBlockType[];
+  };
+
+
+  export type StatisticType = {
+    negative : number,
+    positive : number,
+  };
+
+
+  export type QuestionBlockType = {
+    id: number;
+    blockName: string;
+  };
